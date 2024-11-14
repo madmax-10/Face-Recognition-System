c1022928e639c988e09b7e9801b8b0ead92cc761
madmax-10 <139322421+madmax-10@users.noreply.github.com>
2024-11-13 18:07:25 -0600

    Delete Personal Assistant directory

--- Personal Assistant/updateIDs.py
+++
@@ -1,6 +0,0 @@
-# Run this program before running main.py the very first time
-
-import pickle
-knownEncodingsWithIDs={}
-with open("knownIDs.pkl","wb") as fileEncodes:
-    pickle.dump(knownEncodingsWithIDs,fileEncodes)
